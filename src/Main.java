70f1be21ce6a214b2f12df6127bb09ad1e7d83b4
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-11 11:14:43 +0300

    Adding for loop

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,25 @@ public class Main {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         int number = random.nextInt(1,10) + 1;
-        System.out.print("Enter a number between 1 to 10: ");
-        int guessNumber = scanner.nextInt();
+        int guessNumber = 0;
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Enter a number between 1 to 10: ");
+            guessNumber = scanner.nextInt();
+            if (guessNumber == number) {
+                System.out.println("You have won");
+                break;
+            } else if (guessNumber > number) {
+                System.out.println("Value is too big");
+                continue;
+            } else if (guessNumber < number) {
+                System.out.println("Value too small");
+                continue;
+            }
+        }
+        if (guessNumber != number) {
+            System.out.println("Sorry you have lost");
+        }
+
+
     }
 }
