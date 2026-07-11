1ccb21b8f9668d5bd4ff7d981cb88ac00a54486c
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-11 10:52:27 +0300

    Adding user inputs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(1,10) + 1;
+        System.out.print("Enter a number between 1 to 10: ");
+        int guessNumber = scanner.nextInt();
     }
 }
